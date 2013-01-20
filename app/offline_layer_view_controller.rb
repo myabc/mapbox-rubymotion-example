27c63aefe2138009eeb7394dc4012e16fc69295b
Alex Coles <alex@alexbcoles.com>
2013-01-20 13:17:51 +0100

    Re-open RMMBTilesSource to add yet-to-be-released initializer
    Signed-off-by: Alex Coles <alex@alexbcoles.com>

--- app/offline_layer_view_controller.rb
+++ app/offline_layer_view_controller.rb
@@ -1,23 +1,24 @@
+# XXX: This initializer was addded to the SDK in commit e0f2ea4 and is not yet
+# available as a released CocoaPod.
+# See https://github.com/mapbox/mapbox-ios-sdk/commit/e0f2ea44bf58dca7fb83a977ede71f3fff08eb80
+RMMBTilesSource.class_eval do
+  def initWithTileSetResource(name, ofType:extension)
+    self.initWithTileSetURL(NSURL.fileURLWithPath(NSBundle.mainBundle.pathForResource(name, ofType:extension)))
+  end
+end
+
 class OfflineLayerViewController < UIViewController
 
   def viewDidLoad
     super
 
-    # Convenience method available from mapbox-ios-sdk commit e0f2ea4
-    #offline_source  = RMMBTilesSource.alloc.initWithTileSetResource('control-room-0.2.0', ofType:'mbtiles')
-    offline_source  = init_tiles_source_with_tile_set_resource('control-room-0.2.0', ofType:'mbtiles')
+    offline_source  = RMMBTilesSource.alloc.initWithTileSetResource('control-room-0.2.0', ofType:'mbtiles')
     map_view        = RMMapView.alloc.initWithFrame(self.view.bounds, andTilesource:offline_source)
     map_view.zoom   = 2
     map_view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth
     map_view.adjustTilesForRetinaDisplay = true
 
     self.view.addSubview(map_view)
-  end
-
-  private
-
-  def init_tiles_source_with_tile_set_resource(name, ofType:extension)
-    RMMBTilesSource.alloc.initWithTileSetURL(NSURL.fileURLWithPath(NSBundle.mainBundle.pathForResource(name, ofType:extension)))
   end
 
 end
